44acededfc89fcd9e00abe2a981a7ee070a91ba6
raphael008 <raphael008@icloud.com>
2019-03-17 18:37:06 +0800

    delete base repository

--- community-dao/src/main/java/com/github/raphael008/repository/BaseRepository.java
+++
@@ -1,6 +0,0 @@
-package com.github.raphael008.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BaseRepository<E, T, ID> extends CrudRepository<T, ID> {
-}
